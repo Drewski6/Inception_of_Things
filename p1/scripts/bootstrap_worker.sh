375a407ef2ba6e3b0e439231fbf4492c7e3d2954
Drew Pentland <drewtpentland@pm.me>
2026-02-03 19:22:29 +0000

    feat: server creates token and puts it in a home file. also waits to write to the file until after both the API server is up and the token exists.

--- p1/scripts/bootstrap_worker.sh
+++ p1/scripts/bootstrap_worker.sh
@@ -9,13 +9,22 @@ SERVER_HOST=dpentlanS
 WORKER_HOST=dpentlanSW
 SERVER_IP=192.168.56.110
 WORKER_IP=192.168.56.111
-K3S_TOKEN=K10da2103fc3d5bd28b67eaba39ab81f82ffe936c2d32527ab1f821a937a7fcc617::server:e22e441fe129ca227a32d0dad03ab639
+TOKEN_DST_DIR="/home/vagrant/shared"
+TOKEN_DST="${TOKEN_DST_DIR}/token"
+K3S_TOKEN=""
 
 echo "Running bootstrap_worker.sh"
 
-sudo apt update
-sudo apt upgrade -y
+# sudo apt update
+# sudo apt upgrade -y
 sudo apt install -y curl
+
+until ssh -o StrictHostKeyChecking=no -o ConnectTimeout=2 vagrant@"$SERVER_IP" "sudo test -s $TOKEN_DST"; do
+  echo "waiting for server token..."
+  sleep 2
+done
+
+K3S_TOKEN="$(ssh -o StrictHostKeyChecking=no vagrant@"$SERVER_IP" "sudo cat $TOKEN_DST")"
 
 # Install k3s
 curl -sfL https://get.k3s.io | K3S_URL=https://${SERVER_IP}:6443 K3S_TOKEN=${K3S_TOKEN} sh -
