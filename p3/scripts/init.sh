df9e4388320480ff3feeef9fd8d86ba4251eb515
Drew Pentland <drewtpentland@pm.me>
2026-02-18 21:46:10 +0000

    wip: force p3 script to run as sudo

--- p3/scripts/init.sh
+++ p3/scripts/init.sh
@@ -6,21 +6,25 @@
 
 # Exit on error, treat unset vars as errors, print command before executing
 set -eux
+# Ensure the script is being run as sudo
+if [[ "${EUID:-$(id -u)}" -ne 0 ]]; then
+  exec sudo -E bash "$0" "$@"
+fi
 
 ################################################################################
 # Install Docker (from Docker docs)
 ################################################################################
 
 # Remove any existing repos so that old repos don't interfere with new setup.
-sudo apt remove $(dpkg --get-selections docker.io docker-compose docker-compose-v2 docker-doc podman-docker containerd runc | cut -f1)
+apt remove $(dpkg --get-selections docker.io docker-compose docker-compose-v2 docker-doc podman-docker containerd runc | cut -f1)
 # Add Docker's official GPG key:
-sudo apt-get update -y
-sudo apt-get install -y ca-certificates curl
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
+apt-get update -y
+apt-get install -y ca-certificates curl
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
 # Add the repository to Apt sources:
-sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+tee /etc/apt/sources.list.d/docker.sources <<EOF
 Types: deb
 URIs: https://download.docker.com/linux/ubuntu
 Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
@@ -28,12 +32,12 @@ Components: stable
 Signed-By: /etc/apt/keyrings/docker.asc
 EOF
 # Update again in case anything changed
-sudo apt-get update -y
+apt-get update -y
 # Install the latest version of Docker
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 # Verify that Docker is running
 sleep 5
-sudo systemctl --no-pager status docker
+systemctl --no-pager status docker
 # let docker run without sudo (important for using k3d without sudo later on)
 # sudo usermod -aG docker "$USER"
 # newgrp docker
@@ -58,7 +62,7 @@ curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stabl
 sha256sum --check <(awk '{print $1"  kubectl"}' kubectl.sha256)
 rm -f kubectl.sha256
 # Install Binary
-sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 # Verify install is working
 kubectl version --client --output=yaml
 rm -f kubectl
@@ -68,10 +72,12 @@ rm -f kubectl
 ################################################################################
 
 # Create the cluster
-sudo k3d cluster create
+k3d cluster create
 # Verify
-sudo k3d cluster list
+k3d cluster list
 # 
-sudo k3d kubeconfig merge k3s-default --kubeconfig-merge-default
+k3d kubeconfig merge k3s-default --kubeconfig-merge-default
 kubectl config use-context k3d-k3s-default
+# Verify nodes are reachable
+kubectl get nodes
 
