9934228c7ad06a9761adb9bce029800bc7887442
Drew Pentland <drewtpentland@pm.me>
2026-02-19 00:51:25 +0000

    feat: script finished. added some newlines to make things a little more clear

--- p3/scripts/init.sh
+++ p3/scripts/init.sh
@@ -97,9 +97,10 @@ curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/lat
 install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
 rm argocd-linux-amd64
 # Wait for argocd to spin up (not the best way. find a better way?)
-sleep 10
+sleep 30
 # Display default password for admin
-echo -e "Your initial secret for admin is..."
+echo "Your initial secret for admin is..."
 kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
+printf "\n"
 # port-forward the webgui for ArgoCD
 kubectl port-forward service/argocd-server -n argocd 8080:443
