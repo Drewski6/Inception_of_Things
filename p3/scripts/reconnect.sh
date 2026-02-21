78b01eb457a9b21e82d698af2e6fcac8e8196cc2
Drew Pentland <drewtpentland@pm.me>
2026-02-21 23:20:08 +0000

    feat: Added some plain yaml files for kubernetes. Configuration added for initial k3s deployment of wil42 app

--- p3/scripts/reconnect.sh
+++ p3/scripts/reconnect.sh
@@ -5,16 +5,17 @@
 ################################################################################
 
 # Exit on error, treat unset vars as errors, print command before executing
-set -eux
+set -eu
 
 ################################################################################
 # ArgoCD WebGUI Setup & Status
 ################################################################################
 
 # Display default password for admin
-echo "Your initial secret for admin is..."
-kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d && echo
+echo "Your initial secret for admin is: $(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)"
 # View status of ArgoCD
 kubectl get all -n argocd
+# Print command for loggin into argocd
+echo -e "************************************************************************************\n\nYou can log into the argocd cli with the following command:\n\nargocd login localhost:8080 --username admin --password "$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath='{.data.password}' | base64 -d)" --insecure\n\n************************************************************************************"
 # Port-Forward 443 https server in docker container to outside 8080
 kubectl port-forward service/argocd-server -n argocd 8080:443
